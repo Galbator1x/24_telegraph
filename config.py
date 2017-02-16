b3b686285d7be682343a217205af29dbe30ba40e
Ivan Filimonov <snake73w@gmail.com>
2017-02-16 11:19:33 +0700

    split config to dev and prod

--- config.py
+++ config.py
@@ -2,11 +2,20 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = True
 
-CSRF_ENABLED = True
+class Config(object):
+    DEBUG = False
+    CSRF_ENABLED = True
+    DATABASE_URL = os.environ.get('DATABASE_URL',
+                                  'sqlite:///' + os.path.join(basedir, 'app.db'))
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-DATABASE_URL = os.environ.get('DATABASE_URL',
-                              'sqlite:///' + os.path.join(basedir, 'app.db'))
-SQLALCHEMY_DATABASE_URI = DATABASE_URL
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
