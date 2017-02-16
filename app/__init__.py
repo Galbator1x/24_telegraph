a9012aafa5f01081ebc32a532c3d4b9b44ecd7ca
Ivan Filimonov <snake73w@gmail.com>
2017-02-16 11:25:08 +0700

    change config load

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 db = SQLAlchemy(app)
 with app.app_context():
