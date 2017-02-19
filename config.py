ace361b0dd5321943656eacb48780ad62a683400
Ivan Filimonov <snake73w@gmail.com>
2017-02-19 20:49:59 +0700

    use getenv instead of environ.get

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-DEBUG = os.environ.get('DEBUG', False)
-CSRF_ENABLED = os.environ.get('CSRF_ENABLED', True)
-SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS', False)
-SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI',
-                                         'sqlite:///' + os.path.join(basedir, 'app.db'))
+DEBUG = os.getenv('DEBUG', False)
+CSRF_ENABLED = os.getenv('CSRF_ENABLED', True)
+SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', False)
+SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI',
+                                    'sqlite:///' + os.path.join(basedir, 'app.db'))
