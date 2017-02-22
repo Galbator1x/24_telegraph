ea7371e273ce4a34f7211d1059fcb15fdca83638
Ivan Filimonov <snake73w@gmail.com>
2017-02-22 18:38:57 +0700

    update messages for title

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,9 @@ from wtforms.validators import Length, Required
 
 
 class ArticleForm(Form):
-    title = StringField('Title' , [Length(min=1, max=200,
-        message='Title is too small')])
+    title = StringField('Title',
+                        [Required(message='Content is empty'),
+                         Length(max=200,
+                                message='Title must be less than 200 characters')])
     signature = StringField('Signature', [Length(max=255)])
     body = TextAreaField('Story', [Required(message='Content is empty')])
